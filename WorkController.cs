16e55e39eb18a42b5ae4513914381848f2957a90
210630193 <65148108+210630193@users.noreply.github.com>
2020-11-04 16:43:26 +0800

    Add files via upload

---
+++ WorkController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkController : ControllerBase
+    {
+        private readonly IHostingEnvironment hostingEnvironment;
+        
+        public WorkController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Json(DAL.WorkInfo.Instance.GetCount())
+        }
+        [HttpGet("new")]
+        public ActionResult GetNew()
+        {
+            var result = DAL.WorkInfo.Instance.GetNew();
+            if (result.Count() != 0)
+                return Json(Result.Ok(result));
+            else
+                return Json(Result.Err("记录数为0"));
+        }
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            var result = DAL.WorkInfo.Instance.GetModel(id);
+            if (result != null)
+                return Json(Result.Ok(result));
+            else
+                return Json(Result.Err("WorkId不存在"));
+        }
+
+        //10
+        [HttpPost]
+        public ActionResult Post([FromBody]Model.WorkInfo workInfo)
+        {
+            workInfo.recommend = "否";
+            workInfo.workVerify = "待审核";
+            workInfo.uploadTime = DateTime.Now;
+            try
+            {
+                int n = DAL.WorkInfo.Instance.Add(workInfo);
+                return Json(Result.Ok("发布作品成功"));
+            }
+            catch(Exception ex)
+            {
+                if(ex.Message.ToLower().Contains("foreign key"))
+                    if(ex.Message.ToLower().Contains("username"))
+                        return Json(Result.Err("合法用户才能添加记录"));
+                else
+                        return Json(Result.Err("作品所属活动不存在"));
+                else if(ex.Message.ToLower().Contains("null"))
+                    return Json(Result.Err("作品名称、作品图片、上传作品时间、作品审核情况、用户名、是否推荐不能为空"));
+                else
+                    return Json(Result.Err("ex.Message"));
+            }
+        }
+        
+
+        [httpPut]
+    }
+}
